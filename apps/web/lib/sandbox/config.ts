8b5fe83740f98bcc3801aede37d6391b36c57e64
nicoalbanese <gcalbanese96@gmail.com>
2026-01-21 22:47:22 +0000

    update

--- apps/web/lib/sandbox/config.ts
+++ apps/web/lib/sandbox/config.ts
@@ -4,7 +4,7 @@
  */
 
 /** Default timeout for new sandboxes (5 minutes) */
-export const DEFAULT_SANDBOX_TIMEOUT_MS = 60_000;
+export const DEFAULT_SANDBOX_TIMEOUT_MS = 300_000;
 
 /** Duration to extend timeout by when user requests more time (5 minutes) */
 export const EXTEND_TIMEOUT_DURATION_MS = 300_000;
