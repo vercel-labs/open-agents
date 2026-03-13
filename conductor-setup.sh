9e0693843af998b7b811d42d29a879941cfb6b69
nicoalbanese <gcalbanese96@gmail.com>
2026-03-13 14:43:05 +0000

    remove

--- conductor-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-bun install
-cp "$CONDUCTOR_ROOT_PATH/apps/web/.env" apps/web/.env
-
-vc env pull "$CONDUCTOR_ROOT_PATH/.env.local" --cwd "$CONDUCTOR_ROOT_PATH"
-grep "^VERCEL_OIDC_TOKEN=" "$CONDUCTOR_ROOT_PATH/.env.local" >> apps/web/.env
-grep "^BLOB_READ_WRITE_TOKEN=" "$CONDUCTOR_ROOT_PATH/.env.local" >> apps/web/.env
